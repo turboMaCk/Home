385796b7b94dceb9657095d24452d28e8474c195
Marek Fajkus <marek.faj@gmail.com>
2024-09-24 19:13:10 +0200

    add ssh key for desktop

--- config/ssh.nix
+++ config/ssh.nix
@@ -2,6 +2,7 @@
 {
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDgBVB966ZfrwwOloWlOHeBqIbVFpDxj+bIerQy0TgKNpOZbG8qXaP6zkwyCvY0B8Zqjrvj8sDtcKZkX5YG1qfsunmdnCBvZG3oXWjYzaptpJPnhDQz3yWbShCwzlQ0n/YvkpU5zegYpfYUw5dCvI1FV+OCnjsWqrRTX32XGSsvGTPgfFUYOUZz9V5Qn2gh9/eqPCK4eNM9+gJyk+9izXJLHv3Ksyaeul4O2XHrtiDrZ5BifrENjePKP1cqAqQWX5RNZk+LXLUU1QllSodtMqyH0jA3xJndLTKCx2Rx33XzqNNlmIi/h002CXZJciwR+BJGhmw2nTGoRQ3Q7FIbvBkh marek.faj@gmail.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC1wEkx/OhHXDIPF6evmgTvHG0+0iDA8fjQUvmhC9kwR marek.faj@gmail.com"
   ];
 
   services.sshd.enable = true;
