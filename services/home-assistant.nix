331008178323a77a7b1db94b97886f10d7e19315
Marek Fajkus <marek.faj@gmail.com>
2026-09-20 16:02:08 +0200

    some fixes

--- services/home-assistant.nix
+++ services/home-assistant.nix
@@ -24,8 +24,9 @@ in {
     image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
     extraOptions = [
       "--device=/dev/ttyUSB0:/dev/ttyUSB0" # sky connect
-      "--cap-add=NET_ADMIN,NET_RAW" # Allow watching dhcp packets
       "--network=host" # Among other things this mitigates issues with binding bluetooth into the container
+      "--cap-add=NET_ADMIN"
+      "--cap-add=NET_RAW"
     ];
   };
 }
