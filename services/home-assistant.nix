71c52b54516d5712e6ed062a63a85beab3e31bbe
Marek Fajkus <marek.faj@gmail.com>
2026-09-20 15:22:55 +0200

    cleanups

--- services/home-assistant.nix
+++ services/home-assistant.nix
@@ -9,14 +9,6 @@ let
     '';
   };
 in {
-  # As of Home Assistant 2023.12.0 many components started depending on the matter integration.
-  # It unfortunately still relies on OpenSSL 1.1, which has gone end of life in 2023/09.
-  # For home-assistant deployments to work after this release
-  # you most likely need to allow this insecure dependency in our system configuration.
-  #nixpkgs.config.permittedInsecurePackages = [
-    #"openssl-1.1.1w"
-  #];
-
   system.activationScripts.configureHomeAssistant = lib.stringAfter [ "var" ] ''
     mkdir -p /var/lib/containers/storage/volumes/${volume-name}/_data
     cp -f ${config} /var/lib/containers/storage/volumes/${volume-name}/_data/configuration.yaml
@@ -36,22 +28,4 @@ in {
       "--network=host" # Among other things this mitigates issues with binding bluetooth into the container
     ];
   };
-
-  # Declarative configuration
-  # Not using for now
-  #services.home-assistant = {
-    #enable = true;
-    #extraComponents = [
-      ## Components required to complete the onboarding
-      #"esphome"
-      #"met"
-      #"radio_browser"
-      #"homeassistant_sky_connect"
-    #];
-    #config = {
-      ## Includes dependencies for a basic setup
-      ## https://www.home-assistant.io/integrations/default_config/
-      #default_config = {};
-    #};
-  #};
 }
