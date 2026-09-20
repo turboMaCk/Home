c05bcd900681fe7fa890526bb28b516307d11d83
Marek Fajkus <marek.faj@gmail.com>
2026-09-20 13:21:12 +0200

    nixos anywhere

--- devices/rpi5.nix
+++ devices/rpi5.nix
@@ -1,48 +1,70 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+let
+  firmwarePartition = lib.recursiveUpdate {
+    # label = "FIRMWARE";
+    priority = 1;
 
-  # BOOT partition is small, lets save some space
-  # TODO: resize partitions
-  boot.initrd.compressor = "xz";
-  boot.loader.grub.configurationLimit = 3;
-  hardware.deviceTree.filter = "bcm2712-rpi-5-b.dtb";
+    type = "0700";  # Microsoft basic data
+    attributes = [
+      0 # Required Partition
+    ];
 
-  boot.loader.raspberry-pi = {
-    enable = true;
-    # Force 'kernel' bootloader to stop kernelboot-builder from creating nixos-kernels/
-    bootloader = lib.mkForce "kernel";
+    size = "1024M";
+    content = {
+      type = "filesystem";
+      format = "vfat";
+      # mountpoint = "/boot/firmware";
+      mountOptions = [
+        "noatime"
+        "noauto"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=1min"
+      ];
+    };
   };
+
+  espPartition = lib.recursiveUpdate {
+    # label = "ESP";
+
+    type = "EF00";  # EFI System Partition (ESP)
+    attributes = [
+      2 # Legacy BIOS Bootable, for U-Boot to find extlinux config
+    ];
+
+    size = "1024M";
+    content = {
+      type = "filesystem";
+      format = "vfat";
+      # mountpoint = "/boot";
+      mountOptions = [
+        "noatime"
+        "noauto"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=1min"
+        "umask=0077"
+      ];
+    };
+  };
+
+in {
 
   # use mkpasswd to generate
-  users.users.root.initialHashedPassword = "$6$bCVhxN/V3xpX8m7H$5bcuFMx1VEtE6w9Y3zorg8/DKNB/vHL6qmkCuMiO0GR4ARUEdM8KGeq/Px8hkJR/tjIGx5HLcFHe0ktI8NJVD0";
+  users.users.root.initialHashedPassword = "$y$j9T$Sx4lLdN3Tlqf8R1QLfhO2.$Ful6Ne1uwT/W2PRoR7u1enggN.1k/gNY05LzKqMtcr4";
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS";
-    fsType = "btrfs";
-    options = [ "subvol=@root" "compress=zstd" ];
+  boot = {
+    loader.raspberry-pi.bootloader = "kernel";
+    tmp.useTmpfs = true;
   };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-label/NIXOS";
-    fsType = "btrfs";
-    options = [ "subvol=@home" "compress=zstd" ];
-  };
-
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-label/NIXOS";
-    fsType = "btrfs";
-    options = [ "subvol=@nix" "compress=zstd" "noatime" ];
-  };
-
-  fileSystems."/var/log" = {
-    device = "/dev/disk/by-label/NIXOS";
-    fsType = "btrfs";
-    options = [ "subvol=@log" "compress=zstd" ];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/BOOT";
-    fsType = "vfat";
-    options = [ "fmask=0022" "dmask=0022" ];
+  hardware.raspberry-pi.config = {
+    all = {
+      base-dt-params = {
+        i2c = {
+          enable = true;
+          value = "on";
+        };
+      };
+    };
   };
 
   networking = {
@@ -59,11 +81,121 @@
   nix.gc.automatic = true;
   nix.gc.options = "--delete-older-than 30d";
 
-  # boot.tmp.cleanOnBoot = true;
+  ## Disks
+  # https://nixos.wiki/wiki/Btrfs#Scrubbing
+  services.btrfs.autoScrub = {
+    enable = true;
+    interval = "monthly";
+    fileSystems = [ "/" ];
+  };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should11
-  system.stateVersion = "26.05"; # Did you read the comment?
+  fileSystems = {
+    # mount early enough in the boot process so no logs will be lost
+    "/var/log".neededForBoot = true;
+  };
+
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/nvmen1";
+  };
+
+  content.partitions.FIRMWARE =  firmwarePartition {
+    label = "FIRMWARE";
+    content.mountpoint = "/boot/firmware";
+  };
+
+  contents.partitions.ESP = espPartition {
+    label = "ESP";
+    content.mountpoint = "/boot";
+  };
+
+  contents.partitions.swap = {
+    type = "8200";  # Linux swap
+
+    size = "8G";  # RAM
+    content = {
+      type = "swap";
+      resumeDevice = true;  # "hibernation" swap
+      # zram's swap will be used first, and this one only
+      # used when the system is under pressure enough that zram and
+      # "regular" swap above didn't work
+      # https://github.com/systemd/systemd/issues/16708#issuecomment-1632592375
+      # (set zramSwap.priority > btrfs' .swapvol priority > this priority)
+      priority = 2;
+    };
+  };
+
+  contents.partitions.system = {
+    type = "8305"; # Linux ARM64 root
+    size = "100%";
+  };
+
+  contents.partitions.system.content = {
+    type = "btrfs";
+    extraArgs = [ "-f" ]; # Force overwrite EXISTING DATA!!!
+    postCreateHook =
+      let
+        thisBtrfs = config.disko.devices.disk.main.content.partitions.system.content;
+        device = thisBtrfs.device;
+        subvolumes = thisBtrfs.subvolumes;
+
+        makeBlankSnapshot = btrfsMntPoint: subvol:
+          let
+            subvolAbsPath = lib.strings.normalizePath "${btrfsMntPoint}/${subvol.name}";
+            dst = "${subvolAbsPath}-blank";
+            # NOTE: this one-liner has the same functionality (inspired by zfs hook)
+            # btrfs subvolume list -s mnt/rootfs | grep -E ' rootfs-blank$' || btrfs subvolume snapshot -r mnt/rootfs mnt/rootfs-blank
+          in ''
+                    if ! btrfs subvolume show "${dst}" > /dev/null 2>&1; then
+                    btrfs subvolume snapshot -r "${subvolAbsPath}" "${dst}"
+                    fi
+                    '';
+      in ''
+                MNTPOINT=$(mktemp -d)
+                mount ${device} "$MNTPOINT" -o subvol=/
+                trap 'umount $MNTPOINT; rm -rf $MNTPOINT' EXIT
+                ${makeBlankSnapshot "$MNTPOINT" subvolumes."/rootfs"}
+                '';
+  };
+
+  subvolumes = {
+    "/rootfs" = {
+      mountpoint = "/";
+      mountOptions = [ "noatime" ];
+    };
+    "/nix" = {
+      mountpoint = "/nix";
+      mountOptions = [ "noatime" ];
+    };
+    "/home" = {
+      mountpoint = "/home";
+      mountOptions = [ "noatime" ];
+    };
+    "/log" = {
+      mountpoint = "/var/log";
+      mountOptions = [ "noatime" ];
+    };
+    "/swap" = {
+      mountpoint = "/.swapvol";
+      swap."swapfile" = {
+        size = "8G";
+        priority = 3; # (higher number -> higher priority)
+        # to be used after zswap (set zramSwap.priority > this priority),
+        # but before "hibernation" swap
+        # https://github.com/nix-community/disko/issues/651
+      };
+    };
+  };
 }
+
+
+
+
+
+
+      # This value determines the NixOS release with which your system is to be
+      # compatible, in order to avoid breaking some software such as database
+      # servers. You should change this only after NixOS release notes say you
+      # should11
+      system.stateVersion = "26.05"; # Did you read the comment?
+  };
--- flake.nix
+++ flake.nix
@@ -5,6 +5,11 @@
     nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi";
     deploy-rs.url = "github:serokell/deploy-rs";
     deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixos-raspberrypi/nixpkgs";
+    };
+    nixos-anywhere.url = "github:nix-community/nixos-anywhere";
   };
 
   nixConfig = {
@@ -16,7 +21,7 @@
     ];
   };
 
-  outputs = { self, nixpkgs, nixos-raspberrypi, deploy-rs }@inputs:
+  outputs = { self, nixpkgs, nixos-raspberrypi, deploy-rs, disko, nixos-anywhere }@inputs:
     let
       inherit (nixpkgs.lib) nixosSystem;
       # rpi5-boot = import ./images/rpi5-boot.nix;
@@ -30,6 +35,7 @@
         nixpkgs.lib.genAttrs allSystems
           (system: fn {
             pkgs = import nixpkgs { inherit system; };
+            anywhere = nixos-anywhere.packages.${system}.default;
           });
 
       mkImage = nixosConfig: nixosConfig.config.system.build.sdImage;
@@ -37,14 +43,14 @@
       # This is highly advised, and will prevent many possible mistakes
       checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
 
-      devShells = forAllSystems ({ pkgs }: {
+      devShells = forAllSystems ({ pkgs, anywhere }: {
         default = pkgs.mkShell {
           name = "Home-deploy/build-shell";
-          buildInputs = [ pkgs.deploy-rs ];
+          buildInputs = [ pkgs.deploy-rs anywhere ];
         };
       });
 
-      packages = forAllSystems ({ pkgs }: {
+      packages = forAllSystems ({ pkgs, ... }: {
         rpi5-boot = mkImage self.nixosConfigurations.rpi5-boot;
       });
 
@@ -74,15 +80,37 @@
           ];
         };
 
-        rpi5 = nixos-raspberrypi.lib.nixosSystem {
+        rpi5 = nixos-raspberrypi.lib.nixosSystemFull {
           system = "aarch64-linux";
           specialArgs = inputs;
 
-          modules = with nixos-raspberrypi.nixosModules; [
-            raspberry-pi-5.base
-            raspberry-pi-5.page-size-16k
-            raspberry-pi-5.display-vc4
-            raspberry-pi-5.bluetooth
+          modules = [
+            ({ config, pkgs, lib, nixos-raspberrypi, disko, ... }: {
+              imports = with nixos-raspberrypi.nixosModules; [
+                # Hardware configuration
+                raspberry-pi-5.base
+                raspberry-pi-5.page-size-16k
+                raspberry-pi-5.display-vc4
+              ];
+              hardware.raspberry-pi.config = {
+                all = {
+                  base-dt-params = {
+                    i2c = {
+                      enable = true;
+                      value = "on";
+                    };
+                  };
+                };
+              };
+            })
+
+            # Disk formatting
+            disko.nixosModules.disko
+
+            {
+              boot.loader.raspberry-pi.bootloader = "kernel";
+              boot.tmp.useTmpfs = true;
+            }
 
             ({ config, pkgs, lib, ... }: {
               system.nixos.tags = let
@@ -92,48 +120,6 @@
                 cfg.bootloader
                 config.boot.kernelPackages.kernel.version
               ];
-
-              hardware.raspberry-pi.config = {
-                all = { # [all] conditional filter, https://www.raspberrypi.com/documentation/computers/config_txt.html#conditional-filters
-
-                  options = {
-                    # https://www.raspberrypi.com/documentation/computers/config_txt.html#enable_uart
-                    # in conjunction with `console=serial0,115200` in kernel command line (`cmdline.txt`)
-                    # creates a serial console, accessible using GPIOs 14 and 15 (pins
-                    #  8 and 10 on the 40-pin header)
-                    enable_uart = {
-                      enable = true;
-                      value = true;
-                    };
-                    # https://www.raspberrypi.com/documentation/computers/config_txt.html#uart_2ndstage
-                    # enable debug logging to the UART, also automatically enables
-                    # UART logging in `start.elf`
-                    uart_2ndstage = {
-                      enable = true;
-                      value = true;
-                    };
-                  };
-
-                  # Base DTB parameters
-                  # https://github.com/raspberrypi/linux/blob/a1d3defcca200077e1e382fe049ca613d16efd2b/arch/arm/boot/dts/overlays/README#L132
-                  base-dt-params = {
-
-                    # https://www.raspberrypi.com/documentation/computers/raspberry-pi.html#enable-pcie
-                    pciex1 = {
-                      enable = true;
-                      value = "on";
-                    };
-
-                    # PCIe Gen 3.0
-                    # https://www.raspberrypi.com/documentation/computers/raspberry-pi.html#pcie-gen-3-0
-                    pciex1_gen = {
-                      enable = true;
-                      value = "3";
-                    };
-
-                  };
-                };
-              };
             })
 
             ./devices/rpi5.nix
